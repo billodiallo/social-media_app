5f0d4850016838094f380d36d04ce8c27a76a6f2
Billo Diallo <billodiallonet@gmail.com>
2021-03-30 16:48:16 +0300

    Add association belong_to , has_many for comment and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 1000,
+    too_long: '1000 characters in post is the maximum allowed.' }
+    scope :ordered_by_most_recent, -> { order(created_at: :desc) }
   belongs_to :user
-  has_many :comments
+  has_many :comments,  dependent: :destroy
+  has_many :likes, dependent: :destroy
 end

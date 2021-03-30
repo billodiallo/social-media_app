8ee363eedac27ba4a856856f5c83f09cff4ab50f
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-30 15:51:32 +0200

    Add model for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 100,
+    too_long: '100 characters in post is the maximum allowed.' }
   belongs_to :user
   belongs_to :post
 end

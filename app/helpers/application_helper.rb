e5bf4838eef066df10e10575d755b1b35a9fd12f
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-30 11:08:04 +0200

    Add model and navbar for the user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def user_sessions
+    if user_signed_in?
+
+      content_tag(:div, class: 'd-flex special') do
+        (button_to 'Sign Out', destroy_user_session_path, method: :delete, class: 'btn_all bg-primary') +
+          (link_to 'New Event', new_event_path, method: :delete, class: 'btn_all text-light btn_special1') +
+          (link_to 'Profile', users_index_path, class: 'btn_all text-light btn_special1')
+      end
+
+    else
+
+      (button_to 'Sign In', new_user_session_path, class: 'btn_all bg-primary') +
+        (link_to 'Sign Up', new_user_registration_path, class: 'btn_special1 text-light btn_all')
+
+    end
+  end
 end

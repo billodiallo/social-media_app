5e088224fe0daddd350961c70afd47d33c872fa4
Billo Diallo <billodiallonet@gmail.com>
2021-04-02 16:49:16 +0300

    Move method from controller to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,12 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  # rubocop:disable  Lint/Void
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    # rubocop:disable  Lint/Void
     friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
+    # rubocop:enable  Lint/Void
   end
 
   # Users who have yet to confirme friend requests

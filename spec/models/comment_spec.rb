65bbc4ce8b1f01c344ad96063aaa291005667757
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-04-05 17:10:00 +0200

    Add comment spec for rspec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'ActiveRecord associations' do
-  context 'Associations' do
-    let(:user) { User.new }
-    let(:post) { Post.new }
-    let(:comment) { Comment.create }
-    
-    describe 'associations' do
-      it { should belong_to(:user) }
-      it { should belong_to(:post) }
+    it 'belong to user' do
+      expect { should belong_to(user) }
     end
-
-    describe 'Validations' do
-      it { should validate_presence_of(:content) }
+    it 'belong to post' do
+      expect { should belong_to(post) }
     end
   end
-end
+  describe 'Validations' do
+    it 'validate presence of content' do
+      expect { should validate_presence_of(:content) }
+    end
+  end
 end

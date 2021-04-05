511eec5c2179aade981c14717660200eddf02365
Billo Diallo <billodiallonet@gmail.com>
2021-04-05 18:00:59 +0300

    Add rspec on friendship model

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe Friendship, type: :model do
+  describe 'ActiveRecord associations' do
+    it 'belong to users' do
+      expect { should belong_to(user) }
+    end
+    it 'belong to  friend' do
+      expect { should belong_to(friend) }
+    end
+    
+  end
+end

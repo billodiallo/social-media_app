adfc72f7add3a59310aa167af9af83866bff6085
Billo Diallo <billodiallonet@gmail.com>
2021-04-06 11:26:25 +0300

    Addind unit test in the project

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,11 +27,15 @@ RSpec.describe User, type: :model do
     it 'has many like' do
       expect { should has_many(likes) }
     end
-    it 'has many posts' do
-      expect { should has_many(friendships) }
+    it 'has many  confirmed friendship' do
+      expect { should has_many(confirmed_friendships) }
+    end
+    it 'has many friends' do
+      expect { should has_many(friends) }
     end
     it 'has many inverse_friendships' do
       expect { should has_many(inverse_friendships).with_foreign_key }
     end
   end
 end
+
